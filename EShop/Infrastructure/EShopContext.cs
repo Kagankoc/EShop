e06ec7ed341e4012ea6fb7953d311b05c93a8b89
Kağan Koç <cemsukagan@hotmail.com>
2020-02-12 12:15:45 +0300

    Db Setting

---
+++ EShop/Infrastructure/EShopContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Infrastructure
+{
+    public class EShopContext : DbContext
+    {
+        public EShopContext(DbContextOptions<EShopContext> options) : base(options)
+        {
+
+        }
+    }
+}
